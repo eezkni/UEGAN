480eaf79c4dc1e604023302c1310fb1384ebae6b
sarvghotra <sssarvjeet27@gmail.com>
2021-11-02 21:53:01 +0000

    Update tester.py acc to latest code

--- tester.py
+++ tester.py
@@ -16,6 +16,8 @@ from tqdm import *
 from data_loader import InputFetcher
 from utils import tensor_to_img
 
+from torch.nn.parallel import DistributedDataParallel
+
 
 class Tester(object):
     def __init__(self, loaders, args):
@@ -30,7 +32,7 @@ class Tester(object):
         # Directories.
         self.model_save_path = os.path.join(args.save_root_dir, args.version, args.model_save_path)
         self.sample_path = os.path.join(args.save_root_dir, args.version, args.sample_path)
-        self.log_path = os.path.join(args.save_root_dir, args.version, args.log_path)
+        self.log_path = args.log_path
         self.test_result_path = os.path.join(args.save_root_dir, args.version, args.test_result_path)
 
         # Build the model and tensorboard.
@@ -41,7 +43,7 @@ class Tester(object):
 
     def test(self):
         """ Test UEGAN ."""
-        self.load_pretrained_model(self.args.pretrained_model)
+        self.load_pretrained_model(self.args.pretrained_model_epoch)
         start_time = time.time()
         test_start = 0
         test_total_steps = len(self.loaders.tes)
@@ -66,26 +68,33 @@ class Tester(object):
         with torch.no_grad():
             for test_step in range(test_start, test_total_steps):
                 input = next(self.fetcher_test)
-                test_real_raw, test_name = input.img_raw, input.img_name
                 test_real_raw, test_real_label, test_name = input.img_raw, input.img_exp, input.img_name
 
                 test_fake_exp = self.G(test_real_raw)
 
                 for i in range(0, denorm(test_real_raw.data).size(0)):
                     save_imgs = denorm(test_fake_exp.data)[i:i + 1,:,:,:]
+
                     img_filename = os.path.basename(test_name[i]).split('.')[0]
-                    save_path = os.path.join(test_save_path, '{:s}_{:0>3.2f}_testFakeExp.png'.format(img_filename, self.args.pretrained_model))
+                    save_path = os.path.join(test_save_path, '{:s}_{:0>3.2f}_testFakeExp.png'.format(img_filename, self.args.pretrained_model_epoch))
+                    save_image(save_imgs, save_path)
+                    if self.args.save_input:
+                        label_save_img = denorm(test_real_label.data)[i:i + 1,:,:,:]
+                        label_save_path = os.path.join(test_save_path, '{:s}_{:0>3.2f}_testOrig.png'.format(img_filename, self.args.pretrained_model_epoch))
+                        save_image(label_save_img, label_save_path)
+
+                        input_save_imgs = denorm(test_real_raw.data)[i:i + 1,:,:,:]
+                        input_save_path = os.path.join(test_save_path, '{:s}_{:0>3.2f}_testInput.png'.format(img_filename, self.args.pretrained_model_epoch))
+                        save_image(input_save_imgs, input_save_path)
 
                     fake_img_rgb = tensor_to_img(save_imgs.detach())
                     test_gen_imgs.append(fake_img_rgb)
-
                     test_real_label = denorm(test_real_label.data)[i:i + 1,:,:,:]
                     real_label_rgb = tensor_to_img(test_real_label.detach())
                     test_label_imgs.append(real_label_rgb)
 
-                    save_image(save_imgs, save_path)
-                    save_imgs_compare = torch.cat([denorm(test_real_raw.data)[i:i + 1,:,:,:], denorm(test_fake_exp.data)[i:i + 1,:,:,:]], 3)
-                    save_image(save_imgs_compare, os.path.join(test_compare_save_path, '{:s}_{:0>3.2f}_testRealRaw_testFakeExp.png'.format(img_filename, self.args.pretrained_model)))
+                    #save_imgs_compare = torch.cat([denorm(test_real_raw.data)[i:i + 1,:,:,:], denorm(test_fake_exp.data)[i:i + 1,:,:,:]], 3)
+                    #save_image(save_imgs_compare, os.path.join(test_compare_save_path, '{:s}_{:0>3.2f}_testRealRaw_testFakeExp.png'.format(img_filename, self.args.pretrained_model_epoch)))
 
                 elapsed = time.time() - start_time
                 elapsed = str(datetime.timedelta(seconds=elapsed))
@@ -104,16 +113,16 @@ class Tester(object):
 
             if self.args.is_test_nima:
                 self.nima_result_save_path = './results/nima_test_results/'
-                curr_nima = calc_nima(test_save_path, self.nima_result_save_path,  self.args.pretrained_model)
+                curr_nima = calc_nima(test_save_path, self.nima_result_save_path,  self.args.pretrained_model_epoch)
                 print("====== Avg. NIMA: {:>.4f} ======".format(curr_nima))
 
             if self.args.is_test_psnr_ssim:
                 self.psnr_save_path = './results/psnr_test_results/'
-                curr_psnr = calc_psnr(test_gen_imgs, test_label_imgs, self.psnr_save_path, self.args.pretrained_model)
+                curr_psnr = calc_psnr(test_gen_imgs, test_label_imgs, self.psnr_save_path, self.args.pretrained_model_epoch)
                 print("====== Avg. PSNR: {:>.4f} dB ======".format(curr_psnr))
 
                 self.ssim_save_path = './results/ssim_test_results/'
-                curr_ssim = calc_ssim(test_gen_imgs, test_label_imgs, self.ssim_save_path, self.args.pretrained_model)
+                curr_ssim = calc_ssim(test_gen_imgs, test_label_imgs, self.ssim_save_path, self.args.pretrained_model_epoch)
                 print("====== Avg. SSIM: {:>.4f}  ======".format(curr_ssim))
 
 
@@ -121,18 +130,20 @@ class Tester(object):
     def build_model(self):
         """Create a generator and a discriminator."""
         self.G = Generator(self.args.g_conv_dim, self.args.g_norm_fun, self.args.g_act_fun, self.args.g_use_sn).to(self.device)
-        self.D = Discriminator(self.args.d_conv_dim, self.args.d_norm_fun, self.args.d_act_fun, self.args.d_use_sn, self.args.adv_loss_type).to(self.device)
-        if self.args.parallel:
+        #self.D = Discriminator(self.args.d_conv_dim, self.args.d_norm_fun, self.args.d_act_fun, self.args.d_use_sn, self.args.adv_loss_type).to(self.device)
+        if self.args.parallel_mode == "dataparallel":
             self.G.to(self.args.gpu_ids[0])
-            self.D.to(self.args.gpu_ids[0])
+            #self.D.to(self.args.gpu_ids[0])
             self.G = nn.DataParallel(self.G, self.args.gpu_ids)
-            self.D = nn.DataParallel(self.D, self.args.gpu_ids)
+            #self.D = nn.DataParallel(self.D, self.args.gpu_ids)
+        #elif self.args.parallel_mode == "ddp":
+        #    self.G = DistributedDataParallel(self.G, device_ids=[torch.cuda.current_device()])
         print("=== Models have been created ===")
 
         # print network
         if self.args.is_print_network:
             self.print_network(self.G, 'Generator')
-            self.print_network(self.D, 'Discriminator')
+            #self.print_network(self.D, 'Discriminator')
 
 
     def print_network(self, model, name):
@@ -150,12 +161,12 @@ class Tester(object):
             # save on GPU, load on GPU
             checkpoint = torch.load(checkpoint_path)
             self.G.load_state_dict(checkpoint['G_net'])
-            self.D.load_state_dict(checkpoint['D_net'])
+            #self.D.load_state_dict(checkpoint['D_net'])
         else:
             # save on GPU, load on CPU
             checkpoint = torch.load(checkpoint_path, map_location=lambda storage, loc: storage)
             self.G.load_state_dict(checkpoint['G_net'])
-            self.D.load_state_dict(checkpoint['D_net'])
+            #self.D.load_state_dict(checkpoint['D_net'])
 
         print("=========== loaded trained models (epochs: {})! ===========".format(resume_epochs))
 
